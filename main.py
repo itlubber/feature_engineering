bc36ac8291affba3983c6c526beedd1b0b74126f
itlubber <1830611168@qq.com>
2023-01-09 19:51:50 +0800

    add argparse

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-from utils import logger
+import argparse
+from utils import logger, seed_everything
 from feature_calculator.sample import *
 from feature_calculator.impala_query import query_all_user_data
+
+
+# 配置相关参数或环境
+# 命令行传参
+# parser = argparse.ArgumentParser(description="特征工程相关参数配置")
+# parser.add_argument('--save', action='store_true', default=False, help="布尔型参数使用")
+# parser.add_argument('--seed', type=int, default=3407, help="随机种子, 保证结果可复现时设置")
+# parser.add_argument('--outputs', type=str, default="outputs/all_user_data.pkl", help="最终结果保存的路径")
+# args = parser.parse_args()
+
+
+# 脚本内部传参
+class args:
+    save = False
+    seed = 3407
+    outputs = "outputs/all_user_data.pkl"
+
+
+seed_everything(args.seed)
 
 
 # 获取建模的所有用户ID、时间、好坏标签、对应征信报告(可选)
 all_user_data = query_all_user_data.query()
 
+
 # 计算指标
 feature_xxx = deal_xxx()
+
 
 # 合并指标
 all_user_data = all_user_data.merge(feature_xxx, how="left", on="zjhm")
 
+
 # 保存指标
-save_pickle(all_user_data, "outputs/all_user_data.pkl")
+save_pickle(all_user_data, args.outputs)
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import re
 import time
 import math
+import random
 import joblib
 import numpy as np
 import pandas as pd
@@ -26,6 +27,17 @@ def save_pickle(obj, file):
         os.makedirs(os.path.dirname(file))
         
     return joblib.dump(obj, file)
+
+
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+
+    # torch.manual_seed(seed)
+    # torch.cuda.manual_seed(seed)
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = True
 
 
 class HiveQuery:
